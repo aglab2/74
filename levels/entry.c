263e6a246d3e620f48897c7c5cdd2957892c62ce
aglab2 <aglab3@gmail.com>
2026-09-26 13:53:09 +0800

    Drop debug

--- levels/entry.c
+++ levels/entry.c
@@ -7,7 +7,7 @@
 
 #include "make_const_nonconst.h"
 
-#define TEST_LEVEL LEVEL_RR
+// #define TEST_LEVEL LEVEL_RR
 
 extern const LevelScript level_main_scripts_entry[];
 const LevelScript level_script_entry[] = {
